0207775d9d055dac399c7bc5b595525e1f3292f6
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-01-18 18:42:06 +0800

    video player added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,11 @@
 import { Stack, Box } from '@mui/material';
 import { ChannelCard, VideoCard } from './';
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return 'Loading...';
+
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' alignItems="start" gap={2}>
+    <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' alignItems="start" gap={2}>
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
